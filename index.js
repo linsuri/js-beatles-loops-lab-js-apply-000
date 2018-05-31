6e8f873e32a7ff8fc586e0f13cae669f7a6df74f
Maneepailin Sriuthenchai <maneepailin@gmail.com>
2018-05-31 03:39:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function theBeatlesPlay(musicianArray, instrumentArray) {
   var arr = []
+  var i = 0
   for (i = 0; i < arr.length ; i++) {
     arr.push(`${musicianArray[i]} plays ${instrumentArray[i]}`)
   }
