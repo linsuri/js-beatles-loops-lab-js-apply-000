3b6ea7ee15b3dc37b49b02c83b581dd51b6dc72f
Maneepailin Sriuthenchai <maneepailin@gmail.com>
2018-05-31 03:40:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ function theBeatlesPlay(musicianArray, instrumentArray) {
   var i = 0
   for (i = 0; i < arr.length ; i++) {
     arr.push(`${musicianArray[i]} plays ${instrumentArray[i]}`)
+    return 
   }
   return arr
 }
